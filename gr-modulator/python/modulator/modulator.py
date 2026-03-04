f4513f4f272dc648e8b3efeec6fc8c6b33fd0f35
niv karasenty <niv.karas3nty@gmail.com>
2026-03-04 13:02:17 +0200

    added block, needs to try

--- gr-modulator/python/modulator/modulator.py
+++ gr-modulator/python/modulator/modulator.py
@@ -11,7 +11,8 @@ import numpy as np
 from gnuradio import gr
 import bitarray
 
-# This function returns 
+# This function returns a vector of bits for the coresponding chars
+# The bits order is big-endian, meaning the most significant bit of each character comes first in the output vector.
 def str_to_bits(s):
     ords = (ord(c) for c in s)
     shifts = (7, 6, 5, 4, 3, 2, 1, 0)
@@ -19,26 +20,35 @@ def str_to_bits(s):
 
 class modulator(gr.sync_block):
     """
-    docstring for block modulator
+    Modulating some initial string to our modulation scheme. 
     """
     def __init__(self, t, samp_rate, input_str):
         gr.sync_block.__init__(self,
             name="modulator",
             in_sig=None,
             out_sig=[np.float32, ])
-        self.t = t
-        self.samp_rate = sampe_rate
+        self.t = t # Duration of one part (which is 1/3 of a symbol) in seconds
+        self.samp_rate = samp_rate
         self.input_str = input_str
-        self.queue = []
+        self.sps = int(self.samp_rate * self.t * 3) # samples per symbol
+        self.preamble = -1.0
+        self.sample_queue = [self.preamble] # Start with preamble
 
         self.enqueue_from_string()
 
     def enqueue_from_string(self):
         bit_array = str_to_bits(self.input_str)
-        
+        for bit in bit_array:
+            if bit == 0:
+                self.sample_queue.append(1.0, -1.0, -1.0) # '0' is represented by [1.0, -1.0, -1.0]
+            else:
+                self.sample_queue.append(1.0, 1.0, -1.0) # '1' is represented by [1.0, 1.0, -1.0]
 
     def work(self, input_items, output_items):
         out = output_items[0]
-        # <+signal processing here+>
-        out[:] = whatever
+        for i in range(0, len(out) - 1):
+            if self.sample_queue:
+                out[i] = self.sample_queue.pop(0)
+            else:
+                out[i] = 0.0 # Fill the rest with zeros if the queue is empty
         return len(output_items[0])
