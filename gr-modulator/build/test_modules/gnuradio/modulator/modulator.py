9cb45af55818cb899201fd9ac12ac1f4e7c78e7e
niv karasenty <niv.karas3nty@gmail.com>
2026-03-04 12:06:56 +0200

    added block

--- gr-modulator/build/test_modules/gnuradio/modulator/modulator.py
+++ gr-modulator/build/test_modules/gnuradio/modulator/modulator.py
@@ -9,6 +9,13 @@
 
 import numpy as np
 from gnuradio import gr
+import bitarray
+
+# This function returns 
+def str_to_bits(s):
+    ords = (ord(c) for c in s)
+    shifts = (7, 6, 5, 4, 3, 2, 1, 0)
+    return [(o >> shift) & 1 for o in ords for shift in shifts]
 
 class modulator(gr.sync_block):
     """
@@ -19,7 +26,16 @@ class modulator(gr.sync_block):
             name="modulator",
             in_sig=None,
             out_sig=[np.float32, ])
+        self.t = t
+        self.samp_rate = sampe_rate
+        self.input_str = input_str
+        self.queue = []
 
+        self.enqueue_from_string()
+
+    def enqueue_from_string(self):
+        bit_array = str_to_bits(self.input_str)
+        
 
     def work(self, input_items, output_items):
         out = output_items[0]
